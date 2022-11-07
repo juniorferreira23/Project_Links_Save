7128456f66c379275ce4a9b4887584cc98e67743
juniorferreira23 <junior.ferreira@outlook.com>
2022-11-07 18:20:29 -0300

    add function edit, finish CRUD, add function clicks and styling pages

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -4,9 +4,10 @@ const redirectLink = async (req, res) => {
     const title = req.params.title
     try {
         const docLink = await Link.findOne({title})
+        await Link.updateOne({title}, {$inc: {clicks: 1}})
         res.status(200).redirect(docLink.url)
-    } catch (error) {
-        res.status(400).send('<h2>Link não encontrado :(</h2>')
+    } catch (err) {
+        res.status(404).send('<h2>Link não encontrado :(</h2></br>' + err)
     }
 }
 
@@ -15,17 +16,17 @@ const addLink = async (req, res) => {
     try {
         await docLink.save()
         res.status(201).redirect('/all')
-    } catch (error) {
-        res.status(400).render('index', { error, body: docLink })
+    } catch (err) {
+        res.status(400).render('index', { err, body: docLink })
     }
 }
 
 const allLinks = async (req, res) => {
     try {
         const links = await Link.find({})
-        res.status(200).render('all', { links })
-    } catch (error) {
-        res.status(400).send('<h2>Links não encontrados no Banco de dados, verificar se o banco de dados foi iniciado<h2>')
+        res.status(200).render('all', { links, err: false })
+    } catch (err) {
+        res.status(404).send('<h2>Links não encontrados no Banco de dados, verificar se o banco de dados foi iniciado<h2></br>' + error)
     }
 }
 
@@ -34,8 +35,8 @@ const deleteLink = async (req, res) => {
     try {
         await Link.findByIdAndDelete(idLink)
         res.status(200).send(idLink)
-    } catch (error) {
-        res.status(400).send(error)
+    } catch (err) {
+        res.status(404).send(err)
     }
 }
 
@@ -43,12 +44,30 @@ const loadEditLink = async (req, res) => {
     const idLink = req.params.id
     try {
         const doc = await Link.findById(idLink)
-        console.log(doc)
-        res.status(200).render('edit', {body: doc, error: false})
-    } catch (error) {
-        res.status(400).send(error)
+        res.status(200).render('edit', {body: doc, err: false})
+    } catch (err) {
+        res.status(404).send(err)
     }
 }
 
+const editLink = async (req, res) => {
+    const docLink = {}
+    const idLink = req.params.id
+    docLink.id = idLink
+    docLink.title = req.body.title
+    docLink.description = req.body.description
+    docLink.url = req.body.url
+    
+    try {
+        if(docLink.title == '' || docLink.url == ''){
+            throw new Error('Campos Obrigatórios, preencher todos os campos!')
+        }else{
+            await Link.updateOne({ _id: idLink}, docLink)
+            res.redirect('/all')
+        }
+    } catch (err) {
+        res.status(400).render('edit', { body: docLink, err })
+    }
+}
 
-module.exports = { addLink, redirectLink, allLinks, deleteLink, loadEditLink}
+module.exports = { addLink, redirectLink, allLinks, deleteLink, loadEditLink, editLink}
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const linkController= require('../controllers/linkController')
 
-router.get('/', (req, res) => {res.render('index', { error: false, body: {}})})
+router.get('/', (req, res) => {res.render('index', { err: false, body: {}})})
 router.get('/all', linkController.allLinks)
-router.get('/edit/:id', linkController.loadEditLink)
 router.get('/:title', linkController.redirectLink)
+router.get('/edit/:id', linkController.loadEditLink)
 
 router.post('/add', express.urlencoded({ extended: true }), linkController.addLink)
+router.post('/edit/:id', express.urlencoded({ extended: true }), linkController.editLink)
 
 router.delete('/:id', linkController.deleteLink)
 
